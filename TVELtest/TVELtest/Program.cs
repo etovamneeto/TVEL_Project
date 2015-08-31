acf2816077d5b6a68aba7faea2c10ed6916c0cd4
etovamneeto <lovachevss@gmail.com>
2015-08-31 20:55:48 +0300

    Отчет пишется, трудно. Надо делать интерфейс и блок-схемы

--- TVELtest/TVELtest/Program.cs
+++ TVELtest/TVELtest/Program.cs
@@ -16,7 +16,7 @@ namespace TVELtest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1("Калькулятор-уродец (тестовый)"));
+            Application.Run(new Form1("Тестовый интерфейс"));
         }
     }
 }
